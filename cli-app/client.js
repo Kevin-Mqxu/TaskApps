9221bd00ef0b28520966403e0d8501970e040318
Kevin-Mqxu <33708634+Kevin-Mqxu@users.noreply.github.com>
2017-11-22 12:37:50 +1100

    package.json and server.js and client.js

---
+++ cli-app/client.js
@@ -0,0 +1,219 @@
+const program = require('commander');
+const { prompt } = require('inquirer'); 
+
+const axios = require('axios');
+
+program
+.version('0.0.1')
+.description('REST API client program');
+
+//Add task
+program
+.command('add')
+.alias('a')
+.description('add task')
+.action(()=>{
+    var url = '';
+    var data = {};
+    const questions = [
+    {
+        type : 'input',
+        name : 'url',
+        message : 'Enter url ...',
+        default: 'http://127.0.0.1:8081/tasks'
+    },
+    {
+        type : 'input',
+        name : 'title',
+        message : 'Enter title ...',
+        default: 'task1'
+    },
+    {
+        type : 'input',
+        name : 'description',
+        message : 'Enter description ...',
+        default: 'new task'
+    },
+    {
+        type : 'list',
+        name : 'completed',
+        message : 'Choose status ...',
+        choices: ['true', 'false'],
+        default: 1
+    }
+    ];
+    
+    prompt(questions)
+    .then(answers => {
+        url = answers.url;
+        data.title = answers.title;
+        data.description = answers.description;
+        data.completed = answers.completed;
+
+        axios
+        .post(url, data)
+        .then(function(response){
+            console.log(response.data);
+        })
+        .catch(function(error){
+            console.log(error);
+        });          
+    });
+})
+
+//get task
+program
+.command('get')
+.alias('g')
+.description('get a specific or all tasks ')
+.action(() => {
+    var questions = [
+        {
+            type : 'input',
+            name : 'url',
+            message : 'Enter url ...',
+            default: 'http://127.0.0.1:8081/tasks'
+        },
+        {
+            type : 'input',
+            name : '_id',
+            message : 'Enter id (all/specific id)...',
+            default: 'all'
+        }
+    ];
+    
+    prompt(questions)
+    .then(answers => {
+        var url = answers._id === 'all'? answers.url : answers.url + '/' + answers._id;
+        axios
+        .get(url)
+        .then(response => {
+                for (var i = 0; i < response.data.length; i++){
+                    console.log(response.data[i]);
+                }
+        })
+        .catch(err => console.log(err))
+    })
+    .catch(err => {
+        console.log(err);
+    });   
+
+})
+
+//update task
+program
+.command('update')
+.alias('u')
+.description('update task')
+.action(()=>{
+    var questions = [
+        {
+            type : 'input',
+            name : 'url',
+            message : 'Enter url ...',
+            default: 'http://127.0.0.1:8081/tasks'
+        },
+        {
+            type : 'input',
+            name : '_id',
+            message : 'Enter id ...'
+        },
+        {
+            type : 'input',
+            name : 'title',
+            message : 'Enter title ...'
+        },
+        {
+            type : 'input',
+            name : 'description',
+            message : 'Enter description ...'
+        },
+        {
+            type : 'list',
+            name : 'completed',
+            message : 'Choosse status ...',
+            choices: ['true', 'false'],
+            default: 1
+        }
+    ];
+    
+    prompt(questions)
+    .then(answers => {
+        var url = answers.url;
+        var taskNew = {_id: answers._id, title: answers.title, description: answers.description, completed: answers.completed};
+        axios
+        .put(url, taskNew)
+        .then(res => console.log(res.data))
+        .catch(err => console.log(err))
+    })
+    .catch(err => {
+        console.log(err);
+    });          
+})
+
+//del task
+program
+.command('del')
+.alias('d')
+.description('del task with specific id')
+.action(() => {
+    var questions = [
+        {
+            type : 'input',
+            name : 'url',
+            message : 'Enter url ...',
+            default: 'http://127.0.0.1:8081/tasks'
+        },
+        {
+            type : 'input',
+            name : '_id',
+            message : 'Enter id ...'
+        }
+    ];
+    
+    prompt(questions)
+    .then(answers => {
+        var url = answers.url + '/' + answers._id;
+        axios
+        .delete(url)
+        .then(response => console.log(response.data))
+        .catch(err => console.log(err))
+    })
+    .catch(err => {
+        console.log(err);
+    });   
+})
+
+//list service api
+program
+.command('list')
+.alias('l')
+.description('list service APIs')
+.action(() => {
+    var questions = [
+        {
+            type : 'input',
+            name : 'url',
+            message : 'Enter url ...',
+            default: 'http://127.0.0.1:8081/'
+        }
+    ];
+    
+    prompt(questions)
+    .then(answers => {
+        var url = answers.url;
+        axios
+        .get(url)
+        .then(response => {
+            for (var i = 0; i < response.data.length; i++){
+                console.log(response.data[i]);
+            }            
+        })
+        .catch(err => console.log(err))
+    })
+    .catch(err => {
+        console.log(err);
+    });   
+})
+
+program.parse(process.argv);
---
+++ service-app/server.js
@@ -0,0 +1,109 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var jasonParser = bodyParser.json();
+
+var Datastore = require('nedb');
+var db = new Datastore({ filename: './datafile', autoload: true });
+
+//list APIs
+app.get('/', function (req, res) {
+    console.log("Getting APIs.");
+    var apis = [
+        {method: "post", url:"/tasks", data:"new task"},
+        {method: "get", url:"/tasks"},
+        {method: "get", url:"/tasks/:id"},
+        {method: "put", url:"/tasks", data:"new task"},
+        {method: "delete", url:"/tasks/:id"},
+    ];
+    res.send(apis);
+})
+
+//create task
+app.post('/tasks', jasonParser, function(req, res){
+    if (!req.body) return res.status(403).send("Invalid data.");
+    console.log("Creating task:" + req.body);
+    db.insert(req.body, err => {
+        if (!err) {
+            console.log("Created.");
+            res.sendStatus(200);
+        }
+        else {
+            console.log(err);
+            res.status(500).send(err);
+        }
+    });
+})
+
+//get all tasks
+app.get('/tasks', function(req, res) {
+    console.log("Finding all tasks.");
+    db.find({}, function (err, docs) {
+        if (!err) {
+            console.log("Done.");
+            res.status(200).send(docs);
+        }
+        else {
+            console.log(err);
+            res.sendStatus(500);
+        }
+    });
+})
+
+//get a specific task
+app.get('/tasks/:id', function(req, res) {
+    var id = req.params.id;
+    console.log("Finding task with id:" + id);
+    db.find({_id: id}, function (err, docs) {
+        if (!err) {
+            console.log("Done.");
+            res.status(200).send(docs);
+        }
+        else {
+            console.log(err);
+            res.sendStatus(500);
+        }
+    });
+})
+
+//update task
+app.put('/tasks', jasonParser, function(req, res){
+    if (!req.body) return res.sendStatus(400);
+    var data = req.body;
+    console.log("Updating task:" + JSON.stringify(data));
+    db.update({_id: data._id}, data, {}, (err, numReplaced) => {
+        if (!err) {
+            console.log("Updated:" + numReplaced);
+            res.sendStatus(200);
+        }
+        else {
+            console.log(err);
+            res.sendStatus(500);
+        }
+    })
+})
+
+//delete a specific task
+app.delete('/tasks/:id', function(req, res) {
+    var id = req.params.id;
+    console.log("Deleting task with id:" + id);
+    db.remove({_id: id}, { multi: false }, 
+        (err, numRemoved) => {
+            if (!err){
+                console.log("Deleted:" + numRemoved);
+                res.sendStatus(200);
+            }
+            else{
+                console.log(err);
+                res.sendStatus(err);
+            }
+        });
+})
+
+var server = app.listen(8081, 'localhost', function () {
+
+   var host = server.address().address
+   var port = server.address().port
+
+   console.log("Example app listening at http://%s:%s", host, port)
+})
